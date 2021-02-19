f638502f3562df70a8c1833c958ab4136a130919
GridwatcherPi <mark.krautheim@gmail.com>
2021-02-18 21:49:21 -0500

    Add authentication to Tesla gateway
    Required as of Tesla gateway version 20.49

--- gridwatcher.sh
+++ gridwatcher.sh
@@ -16,7 +16,20 @@ export TMP='/home/pi/.alexa-remote-control'
 ALEXARC='/home/pi/github/alexa-remote-control/alexa_remote_control.sh'
 
 IP="$(grep 'IP=' $TESLA/secrets |cut -d'=' -f2)"
-CMD="curl -sk https://$IP/api/system_status/grid_status"
+
+LOGINCMD="curl -s -k -i \
+          -c $TESLA/cookie.txt \
+          -X POST \
+          -H ""Content-Type: application/json"" \
+          -d @$TESLA/creds.json \
+          https://$IP/api/login/Basic"
+
+CMD="curl -s -k \
+    -b $TESLA/cookie.txt \
+    https://$IP/api/system_status/grid_status"
+
+echo "Logging in..."
+$LOGINCMD >/dev/null
 
 GWDOWNCOUNT=1
 RETRYINTERVAL=10  # When the gateway is down
@@ -30,6 +43,15 @@ do
     # Found fix at: https://stackoverflow.com/questions/24777597
     HOUR=$((10#$(date "+%H")))
     MIN=$((10#$(date "+%M")))
+
+    # Get a new token every hour (or maybe 2, if we don't land
+    # on zero because of processing time)
+    if [[ $MIN -eq 42 ]]
+    then
+        echo "$TIMESTAMP: Getting new gateway token..."
+        $LOGINCMD >/dev/null
+    fi
+
     GSTATUS="$($CMD | jq -r '.grid_status')"
 
     echo "$TIMESTAMP,$GSTATUS" >> $OUTFILE
@@ -40,7 +62,12 @@ do
         if [[ (($(($GWDOWNCOUNT % $RETRYINTERVAL)) == 0)) ]]
         then
             # Next loop
-            $ALEXARC -e "speak: 'It looks like the Tesla gateway has dropped off the Wifi network. I'll try again in ${RETRYINTERVAL} minutes, or you can go out on the porch, open the large cover, and push the reset button with a pencil'"
+            echo "$TIMESTAMP: Announcing network drop..."
+            $ALEXARC -e "speak: \
+               'It looks like the Tesla gateway has dropped off the \
+                wifi network. I'll try again in ${RETRYINTERVAL} \
+                minutes, or you can go out on the porch, open the \
+                large cover, and push the reset button with a pencil'"
         fi
         sleep $SLEEP
         GWDOWNCOUNT=$(($GWDOWNCOUNT + 1))
@@ -51,8 +78,10 @@ do
 
     if [[ $HOUR -eq 19 && $MIN -eq 13 ]]
     then
-        echo "Announcing test..."
-        $ALEXARC -e "speak: 'This is your daily test of the Clowder Cove gridwatcherPEYE system. This is only a test. And oh...you can watch Jeopardy now.'"
+        echo "$TIMESTAMP: Announcing test..."
+        $ALEXARC -e "speak: \
+           'This is your daily test of the Clowder Cove gridwatcherPEYE \
+           system. This is only a test. And oh...you can watch Jeopardy now.'"
     fi
 
     if [ -d '/dev/usb/' ]; then SILENCER=true; else SILENCER=false; fi
@@ -62,15 +91,21 @@ do
     then
         if ! $SILENCER
         then
-            echo "Announcing grid down..."
-            #$ALEXARC -e "speak: 'Clowder Cove grid is down. SAAAAAD. Unplug the car if it's charging and turn off the heatpump (using the power button on the remote) to conserve the Powerwall. You can turn on the gas heater if your ass gets too cold. You can silence this message by plugging any USB device into the gridwatcherpeye.'"
-            $ALEXARC -e "speak: 'Clowder Cove grid is down. SAAAAAD. Unplug the car if it's charging and turn off the heatpump to conserve the Powerwall.'"
+            echo "$TIMESTAMP: Announcing grid down..."
+            $ALEXARC -e "speak: \
+               'Clowder Cove grid is down. SAAAAAD. Unplug the car if \
+                its charging and turn off the heatpump to conserve the \
+                Powerwall. You can turn on the gas heater if your ass \
+                gets too cold. You can silence this message by plugging \
+                any USB device into any USB port on the gridwatcherpeye.'"
         fi
     else
         if $SILENCER
         then
-            echo "Announcing silencer reminder..."
-            $ALEXARC -e "speak: 'This is a reminder. Clowder Cove grid is up. Remove the USB silencer dongle from the gridwatcherpeye.'"
+            echo "$TIMESTAMP: Announcing silencer reminder..."
+            $ALEXARC -e "speak: \
+               'This is a reminder. Clowder Cove grid is up. Remove the USB \
+               silencer dongle from the gridwatcherpeye.'"
         fi
     fi
 
